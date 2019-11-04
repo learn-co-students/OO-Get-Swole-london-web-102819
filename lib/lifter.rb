6317aee9016471b90e87ef1c56801c024781b324
github username <github email address>
2019-11-04 15:54:10 +0000

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,41 @@
 class Lifter
   attr_reader :name, :lift_total
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select{|m| m.lifter == self}
+  end
+
+  def gyms
+    memberships.map{|m| m.gym}
+  end
+
+  def self.lifter_count
+    self.all.count
+  end
+
+  def self.average_lift
+    (self.all.reduce(0){|sum, l| sum += l.lift_total}/ lifter_count.to_f).round(2)
+  end
+
+  def memberships_cost
+    memberships.reduce(0){|sum, m| sum += m.cost}
+  end
+
+  def sign_up(cost, gym)
+    Membership.new(cost, gym, self)
+  end
+
+  
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,21 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+l1 = Lifter.new('john',134)
+l2 = Lifter.new('james',156)
+l3 = Lifter.new('ruby',23)
+
+g1 = Gym.new('hoochies')
+g2 = Gym.new('goochies')
+
+m1 = Membership.new(100, g1, l2)
+m2 = Membership.new(399, g1, l3)
+m3 = Membership.new(90, g2, l2)
+m4 = Membership.new(10, g2, l1)
+
+l1.sign_up(13, g1)
+
+
 binding.pry
 
 puts "Gains!"
